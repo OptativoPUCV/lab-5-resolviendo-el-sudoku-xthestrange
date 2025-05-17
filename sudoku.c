833ed4d1a7b46a821f5084e44cab6bd381d7ca73
Your Name <you@example.com>
2025-05-17 00:24:46 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -62,15 +62,13 @@ List* get_adj_nodes(Node* n){
 
                 if(is_valid(newNode)){
                     pushBack(list, newNode);
-                } else {
-                    free(newNode);
-                }
-              } 
+                } 
               return list;
-          }
+              }
+          } 
         }
     }
-   return list;
+    return list;
 }
 
 int is_final(Node* n){
